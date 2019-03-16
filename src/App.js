14e41f7fbfea2bd6df5d50f0b1cb27187fdd50a9
nagarajanceg <nagharajang@gmail.com>
2019-03-16 10:50:42 -0700

    add a component to react app

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
+
+import Login from './components/login';
+
 // import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   state = {};
-  componentDidMount() {
-    this.fetchDetails();
-  }
-  fetchDetails = () => {
-    const self = this;
-    fetch('http://localhost:3100/get')
-      .then(data => data.json())
-      .then(res => {
-        // self.state.response = 'texttt';
-        self.setState({
-          response: res[0].label
-        });
-        console.log(res[0]);
-      });
-  };
+  
   render() {
-    let buttonText;
-    if (this.state.response !== undefined) {
-      buttonText = this.state.response;
-    } else {
-      buttonText = 'default State';
-    }
+    
     return (
-      <Button variant="contained" color="primary">
-        {buttonText}
-      </Button>
+      <div>
+        
+        <Login />
+      </div>
     );
-    // <div className="App" />;
   }
 }
 
